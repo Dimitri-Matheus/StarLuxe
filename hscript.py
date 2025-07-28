a0aa142fe71032039d3a21b431977a07275be10c
Dimitri Matheus <dimitrimatheusdeoliveira@gmail.com>
2025-07-27 22:03:49 -0400

    Add settings section
    Introduces a settings modal for managing game paths and configuration.

--- hscript.py
+++ hscript.py
@@ -6,7 +6,7 @@ import os, sys, logging
 from CTkListbox import *
 from CTkMessagebox import CTkMessagebox
 from utils.downloader import download_from_github
-from utils.config import load_config, save_config
+from utils.config import load_config, save_config, default
 from utils.injector import ReshadeSetup
 
 logging.basicConfig(level=logging.INFO)
@@ -114,6 +114,170 @@ class Modal(ctk.CTkToplevel):
         super().wm_iconbitmap(resource_path('assets\\icon/Oniric_brown.ico'))
 
 
+#TODO: Fazer a janela do Settings.json
+class Modal_Config(ctk.CTkToplevel):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.title("Settings")
+        self.geometry("700x530")
+        self.resizable(width=False, height=False)
+        self.settings = settings
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
+        # Path Entry #1
+        self.text_1 = ctk.CTkLabel(self, text="Path Game - Genshin Impact", font=ctk.CTkFont(size=20))
+        self.text_1.grid(row=0, column=0, padx=20, pady=(15, 5), sticky="w")
+
+        self.path_entry_1 = ctk.CTkEntry(self, placeholder_text="C:/Games...", font=ctk.CTkFont(family="Verdana", size=14), )
+        self.path_entry_1.configure(width=478, height=48, corner_radius=8)
+        self.path_entry_1.grid(row=1, column=0, padx=20, pady=(5, 5), sticky="w")
+
+        self.button_1 = ctk.CTkButton(self, text="Browser", font=ctk.CTkFont(family="Verdana", size=14, weight="bold"), command=lambda: self.select_folder(self.path_entry_1))
+        self.button_1.configure(width=135, height=48, corner_radius=8)
+        self.button_1.grid(row=1, column=1, padx=0, pady=(5, 5), sticky="w")
+
+        # Path Entry #2
+        self.text_2 = ctk.CTkLabel(self, text="Path Game - Honkai Star Rail", font=ctk.CTkFont(size=20))
+        self.text_2.grid(row=2, column=0, padx=20, pady=(15, 5), sticky="w")
+
+        self.path_entry_2 = ctk.CTkEntry(self, placeholder_text="C:/Games...", font=ctk.CTkFont(family="Verdana", size=14))
+        self.path_entry_2.configure(width=478, height=48, corner_radius=8)
+        self.path_entry_2.grid(row=3, column=0, padx=20, pady=(5, 5), sticky="w")
+
+        self.button_2 = ctk.CTkButton(self, text="Browser", font=ctk.CTkFont(family="Verdana", size=14, weight="bold"), command=lambda: self.select_folder(self.path_entry_2))
+        self.button_2.configure(width=135, height=48, corner_radius=8)
+        self.button_2.grid(row=3, column=1, padx=0, pady=(5, 5), sticky="w")
+
+        # Path Entry #3
+        self.text_3 = ctk.CTkLabel(self, text="Path Game - Wuthering Waves", font=ctk.CTkFont(size=20))
+        self.text_3.grid(row=4, column=0, padx=20, pady=(15, 5), sticky="w")
+
+        self.path_entry_3 = ctk.CTkEntry(self, placeholder_text="C:/Games...", font=ctk.CTkFont(family="Verdana", size=14))
+        self.path_entry_3.configure(width=478, height=48, corner_radius=8)
+        self.path_entry_3.grid(row=5, column=0, padx=20, pady=(5, 5), sticky="w")
+
+        self.button_3 = ctk.CTkButton(self, text="Browser", font=ctk.CTkFont(family="Verdana", size=14, weight="bold"), command=lambda: self.select_folder(self.path_entry_3))
+        self.button_3.configure(width=135, height=48, corner_radius=8)
+        self.button_3.grid(row=5, column=1, padx=0, pady=(5, 5), sticky="w")
+
+        # Themes
+        self.text_4 = ctk.CTkLabel(self, text="Themes", font=ctk.CTkFont(size=20))
+        self.text_4.grid(row=6, column=0, padx=20, pady=(15, 5), sticky="w")
+
+        self.themes_option = ctk.CTkOptionMenu(self, values=["Default"], font=ctk.CTkFont(family="Verdana", size=14), state="disabled")
+        self.themes_option.configure(width=216, height=44)
+        self.themes_option.grid(row=7, column=0, padx=20, pady=(5,5), sticky="w")
+
+        # Settings Manager
+        self.button_4 = ctk.CTkButton(self, text="Save Config", font=ctk.CTkFont(size=20), command=lambda: self.save_path([self.path_entry_1, self.path_entry_2, self.path_entry_3]))
+        self.button_4.configure(width=0, height=0, fg_color="transparent")
+        self.button_4.grid(row=8, column=0, padx=2, pady=(15, 5), sticky="e")
+
+        self.button_5 = ctk.CTkButton(self, text="Reset Config", font=ctk.CTkFont(size=20), command=lambda: self.reset_config())
+        self.button_5.configure(width=0, height=0, fg_color="transparent")
+        self.button_5.grid(row=8, column=1, padx=2, pady=(15, 5), sticky="w")
+
+        self.path_entry_1.insert(0, settings["Games"]["genshin_impact"]["folder"])
+        self.path_entry_2.insert(0, settings["Games"]["honkai_star_rail"]["folder"])
+        self.path_entry_3.insert(0, settings["Games"]["wuthering_waves"]["folder"])
+        
+
+    def select_folder(self, path_widget):
+        foldername = filedialog.askdirectory(title='Open folder', initialdir='/')
+        if foldername:
+            msbox_info = CTkMessagebox(title="Selected Folder", message=f"{foldername}", icon="assets/icon/Oniric_brown.ico", header=False, sound=True, font=ctk.CTkFont(family="Verdana", size=14), fg_color="gray14", bg_color="gray14", justify="center", wraplength=300, border_width=0)
+            msbox_info.title_label.configure(fg_color="gray14")
+            path_widget.delete(0, "end")
+            path_widget.insert(0, foldername)
+
+
+    def save_path(self, path_widgets: list[ctk.CTkEntry]):
+        errors = []
+        for entry in path_widgets:
+            game_path = entry.get()
+            if not game_path:
+                continue
+
+            setup_reshade = ReshadeSetup(self.settings["Games"], game_path, self.settings["Script"], settings["Packages"])
+            result = setup_reshade.verification()
+            if result["status"] == True:
+                game_code = result["game_code"]
+                self.settings["Games"][game_code]["folder"] = game_path
+            else:
+                errors.append(f"{entry}: {result['message']}")
+        if errors:
+            msbox_error = CTkMessagebox(title="Error", message=result["message"], icon="assets/icon/Oniric_brown.ico", header=False, sound=True, font=ctk.CTkFont(family="Verdana", size=14), fg_color="gray14", bg_color="gray14", justify="center", wraplength=300, border_width=0)
+            msbox_error.title_label.configure(fg_color="gray14")
+            return
+        save_config(self.settings)
+        self.destroy()
+
+    def reset_config(self):
+        save_config(default.copy())
+        self.settings.clear()
+        self.settings.update(default)
+        self.destroy()
+        logging.info("Configurações restauradas para o padrão!")
+        python = sys.executable
+        os.execv(python, [python] + sys.argv)
+    
+    
+    def iconbitmap(self, bitmap):
+        self._iconbitmap_method_called = False
+        super().wm_iconbitmap(resource_path('assets\\icon/Oniric_brown.ico'))
+
+
+
+#! Otimizar isso, para o futuro
+"""
+class Modal_Started(ctk.CTkToplevel):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.title("Start the your game")
+        self.geometry("700x340")
+        self.resizable(width=False, height=False)
+
+        for col in range(3):
+            self.grid_columnconfigure(col, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=0)
+        self.grid_rowconfigure(2, weight=0)
+        self.grid_rowconfigure(3, weight=1)
+
+        self.game_1 = ctk.CTkImage(PIL.Image.open(resource_path("assets\\icon/GI.png")), size=(128, 128))
+        self.game_2 = ctk.CTkImage(PIL.Image.open(resource_path("assets\\icon/HSR.png")), size=(128, 128))
+        self.game_3 = ctk.CTkImage(PIL.Image.open(resource_path("assets\\icon/WuWa.png")), size=(128, 128))
+
+        self.button_1 = ctk.CTkButton(self, text="", image=self.game_1, command=lambda: self.destroy())
+        self.button_1.configure(width=128, height=128, corner_radius=8, fg_color="transparent")
+        self.button_1.grid(row=1, column=0, sticky="n", padx=10)
+
+        self.button_2 = ctk.CTkButton(self, text="", image=self.game_2, command=lambda: self.destroy())
+        self.button_2.configure(width=128, height=128, corner_radius=8, fg_color="transparent")
+        self.button_2.grid(row=1, column=1, sticky="n", padx=10)
+
+        self.button_3 = ctk.CTkButton(self, text="", image=self.game_3, command=lambda: self.destroy())
+        self.button_3.configure(width=128, height=128, corner_radius=8, fg_color="transparent")
+        self.button_3.grid(row=1, column=2, sticky="n", padx=10)
+
+        self.text_1 = ctk.CTkLabel(self, text="GI", font=ctk.CTkFont(size=20))
+        self.text_1.grid(row=2, column=0, sticky="n", pady=(10, 0))
+
+        self.text_2 = ctk.CTkLabel(self, text="HSR", font=ctk.CTkFont(size=20))
+        self.text_2.grid(row=2, column=1, sticky="n", pady=(10, 0))
+
+        self.text_3 = ctk.CTkLabel(self, text="WuWa", font=ctk.CTkFont(size=20))
+        self.text_3.grid(row=2, column=2, sticky="n", pady=(10, 0))
+
+    def iconbitmap(self, bitmap):
+        self._iconbitmap_method_called = False
+        super().wm_iconbitmap(resource_path('assets\\icon/Oniric_brown.ico'))
+"""
+
 class HSRS(ctk.CTk):
     def __init__(self, settings: dict):
         super().__init__()
@@ -142,10 +306,14 @@ class HSRS(ctk.CTk):
             self.pages[PageClass.__name__] = page
             page.grid(row=0, column=0, sticky="nsew")
 
-        if not settings["Games"]["honkai_star_rail"]["folder"]:
-            self.show_page("ConfigPage")
+        initial_page = any(
+            game_folder.get("folder", "").strip()
+            for game_folder in self.settings["Games"].values()
+        )
+        if initial_page:
+            self.show_page("HomePage")
         else:
-            self.show_page("ReshadePage")
+            self.show_page("ConfigPage")
 
     # Manager the pages
     def show_page(self, page_name: str):
@@ -193,7 +361,7 @@ class BasePage(ctk.CTkFrame):
 class HomePage(BasePage):
     def __init__(self, parent, controller):
         super().__init__(parent, controller)
-
+        self.modal = None
         self.frame.update_image(self.frame.char_image_4)
         self.frame.grid(pady=40)
 
@@ -202,11 +370,19 @@ class HomePage(BasePage):
         self.text_2.configure(text="Now launch the game via the program if\n you'd like to play with Reshade")
         self.text_2.grid_configure(pady=(100, 10))
 
-        self.button_1.configure(text="Settings", command="", state="disabled")
+        self.button_1.configure(text="Settings", command=lambda: self.open_modal(), state="normal")
         self.button_1.grid_configure(pady=(40, 10))
 
-        self.button_2.configure(text="Start", command=lambda: ReshadeSetup(settings["Games"]["honkai_star_rail"], settings["Games"]["honkai_star_rail"]["folder"], settings["Script"]).inject_game())
+        #! Ajustar esse botão para exibir uma janela e selecionar o jogo necessário
+        self.button_2.configure(text="Start", command=lambda: ReshadeSetup(settings["Games"]["genshin_impact"], settings["Games"]["genshin_impact"]["folder"], settings["Script"], settings["Packages"]).inject_game())
         self.button_2.grid_configure(pady=(40, 10))
+
+    def open_modal(self):
+        if self.modal is None or not self.modal.winfo_exists():
+            self.modal = Modal_Config(self)
+            self.modal.focus()
+        else:
+            self.modal.focus()
 
 # Second Page
 class ReshadePage(BasePage):
@@ -275,20 +451,18 @@ class ConfigPage(BasePage):
     # Check and saves the path in settings.json
     def save_path(self):
         game_path = self.path_entry.get()
-
-        if not game_path:
-            game_path = self.settings["Games"]["honkai_star_rail"]["folder"]
-        
-        setup_reshade = ReshadeSetup(settings["Games"]["honkai_star_rail"], game_path, settings["Script"])
-        
+        setup_reshade = ReshadeSetup(self.settings["Games"], game_path, self.settings["Script"], settings["Packages"])
         result = setup_reshade.verification()
-        if result == True:
-            self.settings["Games"]["honkai_star_rail"]["folder"] = game_path
+
+        if result["status"] == True:
+            game_code = result["game_code"]
+            self.settings["Games"][game_code]["folder"] = game_path
             save_config(self.settings)
             self.controller.show_page("ReshadePage")
         else:
             msbox_error = CTkMessagebox(title="Error", message=result["message"], icon="assets/icon/Oniric_brown.ico", header=False, sound=True, font=ctk.CTkFont(family="Verdana", size=14), fg_color="gray14", bg_color="gray14", justify="center", wraplength=300, border_width=0)
             msbox_error.title_label.configure(fg_color="gray14")
+
     
     # Function to select the path
     def select_folder(self):
@@ -305,7 +479,7 @@ if __name__ == "__main__":
 
     #* Carrega as configurações antes de executar o aplicativo
     ctk.set_default_color_theme(resource_path(settings["Launcher"]["gui_theme"]))
-    logging.info("Config loaded")
+    logging.info("Settings loaded")
 
     app = HSRS(settings)
     app.mainloop()
--- utils/injector.py
+++ utils/injector.py
@@ -14,29 +14,37 @@ def resource_path(relative_path: str) -> Path:
 
     return Path(base) / relative_path
 
-# TODO: Criar um syslink para Presets e modificar o arquivo Reshade.ini
+
 class ReshadeSetup():
-    def __init__(self, code: dict, base: str, script: dict):
+    def __init__(self, code: dict, base: str, script: dict, package: dict):
+        self.code = code
         self.base = base
         self.script = script
+        self.package = package
 
         self.game_base = Path(self.base)
         self.shaders_src = resource_path(script["shaders_dir"])
         self.ini_src = resource_path(script["reshade_file"])
         self.injector = resource_path(script["injector_file"])
-
-        self.game_info = {
-            "exe": code.get("exe", ""),
-            "subpath": code.get("subpath", "")
-        }
-        self.game_dir = (self.game_base / self.game_info["subpath"]).resolve() if self.game_base else None
-        self.exe_path = self.game_dir / self.game_info["exe"] if self.game_dir else None
+        self.download_src = resource_path(package["download_dir"])
 
     def verification(self):
         try:
             if not self.base or not self.game_base.is_dir():
                 logging.error(f"Caminho do jogo não encontrado {self.game_base} ou game_folder é vazio!")
                 raise FileNotFoundError("Game installation folder not found!")
+            
+            for code_games, cfg in self.code.items():
+                exe_path = (self.game_base / cfg["subpath"] / cfg["exe"]).resolve()
+                if exe_path.is_file():
+                    self.selected_code = code_games
+                    self.game_info = cfg
+                    self.game_dir = exe_path.parent
+                    self.exe_path = exe_path
+                    break
+            else:
+                logging.error("Executável do jogo não encontrado em nenhuma configuração!")
+                raise FileNotFoundError("Game executable not found!")
 
             if not self.script.get("shaders_dir", "") or not self.shaders_src.is_dir():
                 logging.error(f"Pasta de shaders não encontrado {self.shaders_src} ou shaders_dir é vazio!")
@@ -50,27 +58,41 @@ class ReshadeSetup():
                 logging.error(f"Injector.exe não encontrado {self.injector} ou injector_file é vazio!")
                 raise FileNotFoundError("Injector executable not found!")
             
-            if not self.exe_path.is_file():
-                logging.error(f"Executável não encontrado {self.exe_path}")
-                raise FileNotFoundError("Game executable not found!")
-            
         except Exception as e:
             return {
                 "status": False,
                 "message": str(e)
             }
 
-        logging.info(f"All checks passed for {self.game_info['exe']} at {self.game_base}")
-        return True
+        logging.info(f"All checks passed for {self.selected_code} at {self.game_base}")
+        return {
+            "status": True,
+            "game_code": self.selected_code
+        }
 
     def inject_game(self):
-        link_dest = self.game_dir / Path(self.script["shaders_dir"]).name
-        if not link_dest.exists():
+        self.game_info = {
+            "exe": self.code.get("exe", ""),
+            "subpath": self.code.get("subpath", "")
+        }
+        self.game_dir = (self.game_base / self.game_info["subpath"]).resolve()
+        self.exe_path = self.game_dir / self.game_info["exe"] if self.game_dir else None
+
+        link_shader = self.game_dir / Path(self.script["shaders_dir"]).name
+        if not link_shader.exists():
             try:
-                logging.info(f"Criando link simbólico: {link_dest} -> {resource_path(self.script["shaders_dir"])}")
-                link_dest.symlink_to(resource_path(self.script["shaders_dir"]), target_is_directory=True)
+                logging.info(f"Criando link simbólico: {link_shader} -> {resource_path(self.script["shaders_dir"])}")
+                link_shader.symlink_to(resource_path(self.script["shaders_dir"]), target_is_directory=True)
             except Exception:
-                logging.info("Falha ao criar o link simbólico")
+                logging.info("Falha ao criar o link simbólico!")
+
+        link_preset = self.game_dir / self.download_src.name
+        if not link_preset.exists():
+            try:
+                logging.info(f"Criando link simbólico: {link_preset} -> {self.download_src}")
+                link_preset.symlink_to(self.download_src, target_is_directory=True)
+            except Exception:
+                logging.info("Falha ao criar o link simbólico!")
         
         ini_dest = self.game_dir / "ReShade.ini"
         if not ini_dest.is_file():
@@ -102,6 +124,6 @@ class ReshadeSetup():
 
 #! Test functions
 #config = load_config()
-#setup_reshade = ReshadeSetup(config["Games"]["honkai_star_rail"], config["Games"]["honkai_star_rail"]["folder"], config["Script"])
+#setup_reshade = ReshadeSetup(config["Games"]["honkai_star_rail"], config["Games"]["honkai_star_rail"]["folder"], config["Script"], settings["Packages"])
 #setup_reshade.verification()
 #setup_reshade.inject_game()
